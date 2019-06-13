bb5bad3a70634798db51c2b135848682ca491070
YiJieZhu <18112855600@163.com>
2019-06-13 22:41:49 +0800

    网关服务增加过滤器

---
+++ spring-cloud-gateway/src/main/java/com/example/spring/cloud/gateway/filter/TokenFilter.java
@@ -0,0 +1,63 @@
+package com.example.spring.cloud.gateway.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 服务网关 Zuul（过滤器）
+ * @author YiJieZhu
+ */
+@Configuration //表示这个类中定义了Bean，会把这个类中bean加载到spring容器中
+public class TokenFilter extends ZuulFilter {
+
+    /**
+     * 过滤器的类型，它决定过滤器在请求的哪个生命周期中执行。
+     * 这里定义为pre，代表会在请求被路由之前执行。
+     * @return
+     */
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    /**
+     * filter执行顺序，通过数字指定。
+     * 数字越大，优先级越低。
+     * @return
+     */
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    /**
+     * 判断该过滤器是否需要被执行。这里我们直接返回了true，因此该过滤器对所有请求都会生效。
+     * 实际运用中我们可以利用该函数来指定过滤器的有效范围。
+     * @return
+     */
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    /**
+     * 过滤器的具体逻辑
+     * @return
+     */
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        String token = request.getParameter("token");
+        if (token == null || token.isEmpty()) {
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            ctx.setResponseBody("token is empty");
+        }
+        return null;
+    }
+}
