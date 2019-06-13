181dddf23feaad042a3a414c4c4cef4575b79599
YiJieZhu <18112855600@163.com>
2019-06-13 18:42:46 +0800

    增加gateway网关服务

---
+++ spring-cloud-gateway/src/main/java/com/example/spring/cloud/gateway/config/Swagger2Config.java
@@ -0,0 +1,49 @@
+package com.example.spring.cloud.gateway.config;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Swagger-ui 2.9.2 version config class
+ * @author YiJieZhu
+ * @since 2019年6月13日 15点04分
+ */
+
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("SpringCloud网关接口文档")
+                .description("网关测试文档")
+                //服务条款网址
+                .termsOfServiceUrl("https://baidu.com")
+                .contact(new Contact("朱贻杰","http://localhost:8001/swagger-ui.html","18112855600@163.com"))
+                .license("Apache License Version 2.0")
+                .version("1.0")
+                .build();
+    }
+
+}
